0b67046b66c390b7ba52bd10c31918dc35d51b95
WisdomWolf <wisdomwolf@gmail.com>
2023-12-03 15:52:14 -0500

    Adds incomplete solution. for day3

---
+++ day3/solution.py
@@ -0,0 +1,41 @@
+import re
+
+
+SYMBOL_MASK = re.compile(r'[\*/\\\!\@\#\$\%\^\&]')
+DIGIT_MASK = re.compile(r'\d+')
+
+
+def get_indexes(lines, pattern):
+    indexes = []
+    for row, line in enumerate(lines):
+        matches = list(re.finditer(pattern, line))
+        for match in matches:
+            span = match.span()
+            if span[1] - span[0] > 1:
+                indexes.append((row, span))
+            else:
+                indexes.append((row, span[0]))
+
+    return indexes
+
+
+def adj_finder(matrix, position):
+    adj = []
+
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
+            rangeX = range(0, matrix[0])
+            rangeY = range(0, matrix[1])
+
+            (newX, newY) = (position[0]+dx, position[1]+dy)
+            
+            if (newX in rangeX) and (newY in rangeY) and (dx, dy) != (0,0):
+                adj.append((newX, newY))
+    return adj
+
+
+def solution():
+    with open('input.txt') as f:
+        lines = [l.strip() for l in f.readlines()]
+    symbol_indexes = get_indexes(lines, SYMBOL_MASK)
+    digit_indexes = get_indexes(lines, DIGIT_MASK)
