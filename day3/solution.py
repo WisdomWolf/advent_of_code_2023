88878a71b14534a9fc0408d8d183193980ecc3f1
Ryan Beaman <rbeaman@aledade.com>
2023-12-04 15:32:09 -0500

    Adds nearly complete day3 solution

--- day3/solution.py
+++ day3/solution.py
@@ -1,7 +1,7 @@
 import re
 
 
-SYMBOL_MASK = re.compile(r'[\*/\\\!\@\#\$\%\^\&]')
+SYMBOL_MASK = re.compile(r'[\*/\\\!\@\#\$\%\^\&\+\-\_\=]')
 DIGIT_MASK = re.compile(r'\d+')
 
 
@@ -11,10 +11,7 @@ def get_indexes(lines, pattern):
         matches = list(re.finditer(pattern, line))
         for match in matches:
             span = match.span()
-            if span[1] - span[0] > 1:
-                indexes.append((row, span))
-            else:
-                indexes.append((row, span[0]))
+            indexes.append((row, span))
 
     return indexes
 
@@ -42,22 +39,47 @@ def build_matrix(coord_list, max_rows):
     matrix = [[] for _ in range(max_rows)]
     for row, col in coord_list:
         matrix[row].append(col)
+    return matrix
 
 
-def solution():
-    with open('input.txt') as f:
-        lines = [l.strip() for l in f.readlines()]
+def solution(lines):
+    part_number_coords = set()
+    part_numbers = []
+
     total_lines = len(lines)
     total_cols = len(lines[0])
     matrix = total_lines, total_cols
 
     digit_indexes = get_indexes(lines, DIGIT_MASK)
+    print('digit_indexes:', digit_indexes)
     digit_matrix = build_matrix(digit_indexes, total_lines)
+    print('digit_matrix:', digit_matrix)
     symbol_indexes = get_indexes(lines, SYMBOL_MASK)
+    symbol_indexes = [(row, span[0]) for row, span in symbol_indexes]
+
+    print('symbol_indexes:', symbol_indexes)
 
     for index in symbol_indexes:
         adjacent_cells = find_adjacent_cells(matrix, index)
         adjacency_matrix = build_matrix(adjacent_cells, total_lines)
-        for row in adjacency_matrix:
+        for row_number, row in enumerate(adjacency_matrix):
             for col in row:
-                ...
+                possible_adjacent_digits = digit_matrix[row_number]
+                # print('poss adj digits:', possible_adjacent_digits)
+                for span_start, span_end in possible_adjacent_digits:
+                    if span_start <= col <= span_end:
+                        part_number_coords.add((row_number, (span_start, span_end)))
+    
+    for row_number, span in part_number_coords:
+        start, end = span
+        part_number_str = lines[row_number][start:end]
+        part_numbers.append(int(part_number_str))
+    print('part_numbers:', part_numbers)
+    return sum(part_numbers)
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        lines = f.readlines()
+        print(solution(lines))
+
