594b5591cf1844972b175c0f88a8321f3fea66fc
WisdomWolf <wisdomwolf@gmail.com>
2023-12-09 22:00:39 -0500

    Adds day9

---
+++ day9/solution.py
@@ -0,0 +1,42 @@
+
+
+def get_diffs(seq):
+    diffs = []
+    for i in range(len(seq) - 1):
+        diffs.append(seq[i + 1] - seq[i])
+    return diffs
+
+
+def calc_diff_list(seq):
+    diff_list = [seq]
+    diffs = seq
+    while any(diffs):
+        diffs = get_diffs(diffs)
+        if any(diffs):
+            diff_list.append(diffs)
+    return diff_list
+    
+
+def calc_next_value(seq):
+    diff_list = calc_diff_list(seq)
+    # print(f'diff_list: {diff_list}')
+    next_val = diff_list.pop()[0]
+    diff_list.reverse()
+    for diffs in diff_list:
+        next_val += diffs[-1]
+    # print(f'next_val: {next_val} | {seq}')
+    return next_val
+
+
+def solution(lines):
+    val_list = []
+    for line in lines:
+        seq = [int(x) for x in line.strip().split()]
+        val_list.append(calc_next_value(seq))
+    return sum(val_list)
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        lines = f.readlines()
+        print(solution(lines))
