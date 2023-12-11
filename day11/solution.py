06862300948aeff190111c828c583f9942f7ef98
Ryan Beaman <rbeaman@aledade.com>
2023-12-11 16:05:20 -0500

    Adds better method for expanding cols

--- day11/solution.py
+++ day11/solution.py
@@ -36,33 +36,63 @@ def get_empty_rows(df):
     return empty_rows
 
 
-def expand_rows(df, row_list):
+def expand_rows(df, row_list, multiplier=1):
+    df = df.copy()
     for row in row_list:
-        df.loc[row + .5] = ['.'] * df.shape[1]
+        base_increment = 1 / (multiplier + 1)
+        increment = base_increment
+        # print(f'Increment: {increment}')
+        for i in range(multiplier):
+            # print(f'pass: {i}/{multiplier}')
+            # print(f'Inserting new row at {row + increment}')
+            df.loc[row + increment] = ['.'] * df.shape[1]
+            increment = base_increment + increment
     df = df.sort_index().reset_index(drop=True)
     return df
 
 
-def expand_cols(df, col_list):
-    offset = 1
-    df = df.copy()
-    for col in col_list:
-        pos = col + offset
-        df.insert(pos, col + .5, ['.'] * df.shape[0])
-        offset += 1
-
-    col_map = {col: i for i, col in enumerate(df.columns)}
-    df = df.rename(columns=col_map)
-
+def build_empty_col_df(row_length, start, multiplier):
+    dict_list = []
+    for _ in range(row_length):
+        dict_list.append({i: '.' for i in range(start, start + multiplier)})
+    df = pd.DataFrame(dict_list)
     return df
 
+def expand_cols(df, col_list, multiplier=1):
+    df_list = []
+    last = -1
+    offset = 0
+    for col in col_list:
+        base_df = df.iloc[:, last + 1:col]
+        df_list.append(rename_df(base_df, offset))
+        start = col + offset
+        new_df = build_empty_col_df(df.shape[0], start, multiplier)
+        df_list.append(new_df)
+        last = col
+        offset = new_df.columns[-1] - last
+    base_df = df.iloc[:, last + 1:]
+    df_list.append(rename_df(base_df, offset))
+    result_df = pd.concat(df_list, axis=1)
+    return result_df
 
-def expand_df(df):
+
+def rename_df(df, offset):
+    col_map = {
+        orig: new 
+        for orig, new in zip(
+            df.columns,
+            [c + offset for c in df.columns]
+        )
+    }
+    return df.rename(columns=col_map)
+
+
+def expand_df(df, multiplier=1):
     empty_cols = get_empty_cols(df)
-    df = expand_cols(df, empty_cols)
+    df = expand_cols(df, empty_cols, multiplier)
 
     empty_rows = get_empty_rows(df)
-    df = expand_rows(df, empty_rows)
+    df = expand_rows(df, empty_rows, multiplier)
 
     return df
 
@@ -92,9 +122,9 @@ def calc_total_distances(coord_list):
     return total_distance
 
 
-def solution(lines):
+def solution(lines, multiplier=1):
     df = build_df(lines)
-    df = expand_df(df)
+    df = expand_df(df, multiplier)
     coords = get_galaxy_coords(df)
     total_sum = calc_total_distances(coords)
     
